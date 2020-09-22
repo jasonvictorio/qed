8d08cf8abd3f48da45dd41ad95782c6805d9029d
Jason Victorio <jgvictorio@gmail.com>
2020-09-22 15:58:03 +1200

    capitalize h2

---
+++ index.js
@@ -0,0 +1,10 @@
+const capitalizeFirstLetter = string => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+const h2s = Array.from(document.getElementsByTagName('h2'))
+h2s.forEach(h2 => {
+  h2.innerHTML = h2.innerHTML.split(' ')
+    .map(capitalizeFirstLetter)
+    .join(' ')
+})
